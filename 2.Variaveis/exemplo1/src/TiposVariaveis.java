60ba6176e78ae1396cff499257b579eec0bb9bf4
Emerson Oliveira <emerson.oliverbr@gmail.com>
2024-03-02 21:16:31 -0300

    Tipos de Variaveis

---
+++ 2.Variaveis/exemplo1/src/TiposVariaveis.java
@@ -0,0 +1,19 @@
+public class TiposVariaveis {
+	public static void main(String[] args) {
+		double salarioMinimo = 2500.33;
+		byte idade = 38;
+		short ano = 2024;
+		int cep = 14165987;
+		long cpf = 31139062547L;
+		float pi = 3.14f;
+		
+		cep = 14160000;
+
+		System.out.println(salarioMinimo);
+		System.out.println(idade);
+		System.out.println(ano);
+		System.out.println(cep);
+		System.out.println(cpf);
+		System.out.println(pi);
+	}
+}
